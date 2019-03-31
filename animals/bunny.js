65b6bab36c4f530c7a7b4548e34f90930821950e
Andrew Shaud <andrewshaud@Andrews-Mini.eau.wi.charter.com>
2019-03-31 16:08:12 -0400

    E+S: bunny moves toward carrot smell

--- animals/bunny.js
+++ animals/bunny.js
@@ -1,26 +1,51 @@
 function Bunny(options) {
 
   function Think(forest) {
+    
+    //stimulus = forest.stimulate(this.senses); 
+    
+    stimulus = {};
+    stimulus.smells= [
+      {desc:"poo", x:100, y:200}, {desc:"carrot", x:300, y:100}
+    ];
+    stimulus.sights =[
+      {desc:"carrot", x:200, y:200}
+    ];
+
+    smells = stimulus.smells;
+    sights = stimulus.sights;
+    
+	this.desired_direction.x = smells[1].x - this.pos.x;
+	this.desired_direction.y = smells[1].y - this.pos.y;
+  };
+
+  function Act() {
     var oldpos = {};
     oldpos.x = this.pos.x;
     oldpos.y = this.pos.y;
 
-    var t = forest.timestep();
-    this.pos.x = 400 + 100*Math.cos(t/40);
-    this.pos.y = 400 + 100*Math.sin(t/40);
-    
-    this.dir = get_direction(oldpos,this.pos);
-    
+    speed = this.speed;
+    dir = this.getDesiredDirection();
+    this.pos.x = oldpos.x + speed*dir.x;
+    this.pos.y = oldpos.y + speed*dir.y;     
+
+    this.facing = get_direction(oldpos,this.pos);   
     this.ChooseSprite();
+    this.sprites[this.current_sprite].update(this.pos);
+    this.sprites[this.current_sprite].render();
   };
 
   function ChooseSprite() {
-    this.current_sprite = this.dir.x < 0 ? 0 : 1;
+    this.current_sprite = this.facing.x < 0 ? 0 : 1;
   };
-
-  function Act() {
-    this.sprites[this.current_sprite].update(this.pos);
-    this.sprites[this.current_sprite].render();
+  
+  function getDesiredDirection() {
+    x = this.desired_direction.x;
+    y = this.desired_direction.y;
+    dir_length = Math.sqrt(x*x+y*y)
+    r_x = x/dir_length;
+    r_y = y/dir_length;
+    return { x:r_x, y:r_y };
   };
     
   var bunny = {};
@@ -28,13 +53,18 @@ function Bunny(options) {
   bunny.sprites = [];
 
   bunny.pos = {};
-  bunny.pos.x = 100;
-  bunny.pos.y = 100;
+  bunny.pos.x = 300;
+  bunny.pos.y = 300;
 
-  bunny.dir = {};
-  bunny.dir.x = 1;
-  bunny.dir.y = 0;
+  bunny.facing = {};
+  bunny.facing.x = 1;
+  bunny.facing.y = 0;
 
+  bunny.speed = 1;
+  bunny.desired_direction = {};
+  bunny.desired_direction.x = 1
+  bunny.desired_direction.y = 1
+   
   bunny.current_sprite = 0;
 
   var num_sprites = 2;
@@ -64,8 +94,10 @@ function Bunny(options) {
 
   bunny.Think = Think;
   bunny.ChooseSprite = ChooseSprite;
+  bunny.getDesiredDirection = getDesiredDirection;
   bunny.Act = Act;
 
-    return bunny;       
+    return bunny;  
+         
 };
 
